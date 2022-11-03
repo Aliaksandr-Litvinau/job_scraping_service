e45eb397a72b34b8a42c0e08aa15686e39d29666
Aliaksandr-Litvinau <litvinaualex@gmail.com>
2022-11-03 21:07:39 +0300

    add PL, Vacancy models

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
@@ -11,3 +12,40 @@ class City(models.Model):
     class Meta:
         verbose_name = "City"
         verbose_name_plural = "Cities"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.slug)
+        super(City, self).save(*args, **kwargs)
+
+
+class ProgrammingLanguage(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Programming language"
+        verbose_name_plural = "Programming languages"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.slug)
+        super(ProgrammingLanguage, self).save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=250)
+    company = models.CharField(max_length=250)
+    description = models.TextField()
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    prog_lang = models.ForeignKey('ProgrammingLanguage',
+                                  on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Vacancy"
+        verbose_name_plural = "Vacancies"
