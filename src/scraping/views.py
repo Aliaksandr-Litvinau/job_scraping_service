6489797389c926260b6d316bc7a40af292e7c4a6
Aliaksandr-Litvinau <litvinaualex@gmail.com>
2022-11-04 18:17:30 +0300

    add cards in home.html, <p, <small, |date

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -5,9 +5,3 @@ from .models import Vacancy
 def home_view(request):
     query_set = Vacancy.objects.all()
     return render(request, "scraping/home.html", {"object_list": query_set})
-
-# def home(request):
-#     date = datetime.now().date()
-#     name = 'Dave'
-#     _context = {'date': date, 'name': name}
-#     return render(request, 'home.html', _context)
