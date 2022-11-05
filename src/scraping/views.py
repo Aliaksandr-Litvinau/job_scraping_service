ad47dedc00b568dde84e52c310af1381f79b5fd9
Aliaksandr-Litvinau <litvinaualex@gmail.com>
2022-11-05 12:01:35 +0300

    add filter in home_view, form in home.html

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -3,6 +3,14 @@ from .models import Vacancy
 
 
 def home_view(request):
-    print(request.POST)
-    query_set = Vacancy.objects.all()
+    city = request.GET.get('city')
+    prog_lang = request.GET.get('prog_lang')
+    query_set = []
+    if city or prog_lang:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if prog_lang:
+            _filter['prog_lang__slug'] = prog_lang
+        query_set = Vacancy.objects.filter(**_filter)
     return render(request, "scraping/home.html", {"object_list": query_set})
